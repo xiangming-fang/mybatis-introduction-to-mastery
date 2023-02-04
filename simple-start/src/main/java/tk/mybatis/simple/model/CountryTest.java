f4280f51c2a3c222a5cb37c92610edc3a9b0ec01
xiangming.fang <fang.albert@foxmail.com>
2023-02-04 18:58:54 +0800

    simple-start 步骤注释

--- simple-start/src/main/java/tk/mybatis/simple/model/CountryTest.java
+++ simple-start/src/main/java/tk/mybatis/simple/model/CountryTest.java
@@ -16,13 +16,17 @@ import java.util.List;
  */
 public class CountryTest {
     public static void main(String[] args) throws IOException {
+        // 1、读取mybatis-config文件，生成reader
         Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        // 2、sqlsessionFactoryBuilder 根据 reader 生成sqlSessionFactory 对象
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
         reader.close();
+        // 3、根据sqlSessionFactory生成sqlSession
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        List<Country> list = sqlSession.selectList("selectAll");
+        // 4、sqlSession调用执行的方法，这方法定义在 mapper 文件里
+        List<Country> list = sqlSession.selectList("selectAllCountry");
         for (Country country : list) {
-            System.out.println(country);
+            System.out.println(country.toString());
         }
         sqlSession.close();
     }
