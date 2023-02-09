67e2f55f168db9f474513a5ce16a3c0fca75f9a0
xiangming.fang <fang.albert@foxmail.com>
2023-02-09 22:08:33 +0800

    generator - 自动生成代码架构

---
+++ generator/src/main/java/tk/mybatis/generator/pojo/BasePO.java
@@ -0,0 +1,22 @@
+package tk.mybatis.generator.pojo;
+
+/**
+ * @ProjectName: mybatis
+ * @Package: tk.mybatis.generator.pojo
+ * @ClassName: BasePO
+ * @Author: albert.fang
+ * @Description: 生成的所有实体类基类
+ * @Date: 2023/2/9 22:04
+ */
+public class BasePO {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
